8c22e3200af7f1a193c5948c796b0d520fa560d9
fawadsuhail <fawadsuhail@gmail.com>
2017-01-22 14:48:00 +0400

    Initial commit

---
+++ TwilioTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwilioTest
+//
+//  Created by fawad on 22/01/2017.
+//  Copyright © 2017 fawad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
